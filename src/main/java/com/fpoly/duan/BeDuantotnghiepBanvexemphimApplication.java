3cd830088349d5758eb23838198edf7912169bd7
NguyenThanhCong004 <ntcong0204@gmail.com>
2026-03-10 20:09:31 +0700

    first commit

---
+++ src/main/java/com/fpoly/duan/BeDuantotnghiepBanvexemphimApplication.java
@@ -0,0 +1,13 @@
+package com.fpoly.duan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeDuantotnghiepBanvexemphimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeDuantotnghiepBanvexemphimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpoly/duan/BeDuantotnghiepBanvexemphimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpoly.duan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeDuantotnghiepBanvexemphimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
